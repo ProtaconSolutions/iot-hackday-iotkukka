cbc309d7a4db7aacfc5e3fb1a077410da25f7c79
Jenni Rossi <jenni.rossi@protacon.com>
2015-11-05 16:51:56 +0200

    Initial commit

---
+++ src/app/app.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('iot', [
+      'iot.config',
+      'iot.core',
+      'iot.about',
+      'iot.layout',
+      'iot.demo'
+    ])
+  ;
+})();
